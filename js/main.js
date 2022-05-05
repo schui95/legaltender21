527cc29871eb97d7e0ef9cb361bca1118dbae517
schui95 <schui95@bluewin.ch>
2022-05-06 00:06:12 +0200

    Show countries in color which are approving btc

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 let adoptions = {
     "El Salvador": {rank: 1, blockheight: 686380, source: "https://www.bbc.com/news/world-latin-america-57398274"}, 
-    "Central African Rep.": {rank: 2, blockheight: 733777, source: "https://www.bbc.com/news/world-africa-61248809"}
+    "Central African Rep.": {rank: 2, blockheight: 733777, source: "https://www.bbc.com/news/world-africa-61248809"},
+    "Panama": {source: "https://fortune.com/2022/04/29/panama-legalize-use-bitcoin-cryptocurrencies-legal-tender/"}
 }
 
 fetch("https://raw.githubusercontent.com/schui95/legaltender21/main/resources/countries.json").then((r) => r.json()).then((data) => {
@@ -12,7 +13,7 @@ fetch("https://raw.githubusercontent.com/schui95/legaltender21/main/resources/co
             datasets: [{
                 label: "Countries",
                 data: countries.map((c) => ({feature: c, value: (c.properties.name in adoptions)? adoptions[c.properties.name].blockheight:-1})),
-                backgroundColor: countries.map((c) => (c.properties.name in adoptions)? "#F2A900": "#282828")
+                backgroundColor: countries.map((c) => (c.properties.name in adoptions)? (adoptions[c.properties.name].rank)? "#F2A900": pattern.draw("diagonal", "#282828", "#739DFF", 6) : "#282828")
             }]
         },
         options: {
@@ -32,14 +33,14 @@ fetch("https://raw.githubusercontent.com/schui95/legaltender21/main/resources/co
                     callbacks: {
                         label: function(context) {
                             let country = context.element.feature.properties.name;
-                            if (country in adoptions) {
+                            if (country in adoptions && adoptions[country].rank) {
                                 return `${country} #${adoptions[country].rank}`;
                             }
                             return country;
                         },
                         afterBody: function(context) {
                             let country = context[0].element.feature.properties.name;
-                            if (country in adoptions) {
+                            if (country in adoptions && adoptions[country].blockheight) {
                                 return `Block Height: ${adoptions[country].blockheight}`;
                             }
                             return "";
